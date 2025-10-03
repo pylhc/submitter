9673672daf10d6f01779b1dd7d9a44b11f025d09
Felix Soubelet <felix.soubelet@protonmail.com>
2025-10-03 16:53:09 +0200

    patch version

--- pylhc_submitter/__init__.py
+++ pylhc_submitter/__init__.py
@@ -11,7 +11,7 @@ pylhc-submitter contains scripts to simplify the creation and submission of jobs
 __title__ = "pylhc_submitter"
 __description__ = "pylhc-submitter contains scripts to simplify the creation and submission of jobs to HTCondor at CERN"
 __url__ = "https://github.com/pylhc/submitter"
-__version__ = "2.0.4"
+__version__ = "2.0.5"
 __author__ = "pylhc"
 __author_email__ = "pylhc@github.com"
 __license__ = "MIT"
